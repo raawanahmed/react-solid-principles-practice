792899f9ffb3c9ae4264800dd222969ec3c6b086
Rawan Ahmed <62115135+raawanahmed@users.noreply.github.com>
2024-12-02 19:59:18 +0200

    clean ocp

--- src/principles/OCP/index.tsx
+++ src/principles/OCP/index.tsx
@@ -1,11 +1,4 @@
-import { Button } from "./button";
-import {
-  HiOutlineArrowNarrowRight,
-  HiOutlineArrowNarrowLeft,
-} from "react-icons/hi";
-import { Good } from "./good";
 import { Bad } from "./bad";
-import { GPT4 } from "./GPT4";
 
 export function OCP() {
    return <Bad />;
