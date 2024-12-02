8c0c6ddda5930135e152b6d6560033a1efb574e6
Rawan Ahmed <62115135+raawanahmed@users.noreply.github.com>
2024-12-02 19:37:31 +0200

    clear

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { LSP } from "./principles/LSP";
 import { OCP } from "./principles/OCP";
 import { SRP } from "./principles/SRP";
 
-//Advanced 
+//Advanced
 import { SRP_ADVANCED } from "./advanced/SRP";
 import { OCP_ADVANCED } from "./advanced/OCP";
 import { LSP_ADVANCED } from "./advanced/LSP";
@@ -13,19 +13,21 @@ import { DIP_ADVANCED } from "./advanced/DIP";
 
 function App() {
   return (
-    <div className="flex min-w-full h-full justify-center items-center p-8">
-      <SRP />
-      {/* <OCP />  */}
-      {/* <LSP /> */}
-      {/* <DIP />
-    </div>
-    <div className="flex min-w-full h-full justify-center items-center p-8">
-      {/* <SRP_ADVANCED /> */}
-      {/* <OCP_ADVANCED /> */}
-      {/* <LSP_ADVANCED /> */}
-      {/* <ISP_ADVANCED /> */}
-    {/* <DIP_ADVANCED /> */}
-    </div>
+    <>
+      <div className="flex min-w-full h-full justify-center items-center p-8">
+        <SRP />
+        {/* <OCP />
+        <LSP />
+        <DIP /> */}
+      </div>
+      {/* <div className="flex min-w-full h-full justify-center items-center p-8">
+        <SRP_ADVANCED />
+        <OCP_ADVANCED />
+        <LSP_ADVANCED />
+        <ISP_ADVANCED />
+        <DIP_ADVANCED />
+      </div> */}
+    </>
   );
 }
 
